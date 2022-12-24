de418d0ce76dff91d3d93dde3e9e9b3cd7712657
voteslaugh <usualvlad@gmail.com>
2022-12-24 05:16:15 +0500

    Изменения в RandomizerTest

--- src/main/java/bot/tests/RandomizerTest.java
+++ src/main/java/bot/tests/RandomizerTest.java
@@ -2,30 +2,27 @@ package bot.tests;
 
 import bot.functions.Randomizer;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static bot.functions.Randomizer.getLongInRange;
+import static bot.functions.Randomizer.getRandomCollectionElement;
+
 class RandomizerTest {
     Randomizer randomizer;
-
-    @BeforeEach
-    void prepareData() {
-        randomizer = new Randomizer();
-    }
 
     @Test
     void getLongInRangeTest() {
         int min = -1000, max = 1000;
-        long randomDigit = randomizer.getLongInRange(min, max);
+        long randomDigit = getLongInRange(min, max);
         Assertions.assertTrue(min <= randomDigit && randomDigit <= max);
     }
 
     @Test
     void getRandomIndexTest() {
         Short[] someList = new Short[] {0, 1};
-        long randomIndex = randomizer.getRandomIndex(someList.length);
-        Assertions.assertTrue(Arrays.asList(someList).contains(someList[(int) randomIndex]));
+        Short randomElement = (Short) getRandomCollectionElement(someList);
+        Assertions.assertTrue(Arrays.asList(someList).contains(randomElement));
     }
 }
